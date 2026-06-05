e5d1349fda7e3c6625f2d4609691bd13e32ebff6
Vedansh Upadhyay <ru20567@gmail.com>
2026-06-05 12:03:56 +0530

    Add files via upload
    binary search

---
+++ practice.java
@@ -0,0 +1,18 @@
+public class claude {
+
+    public static void main(String[] args) {
+        
+        int target = 9;
+        int arr[] = {-1, 0, 2, 3, 5, 9, 12};
+            int n  = arr.length-1;
+        int j =0;
+
+        while(j <=1)
+
+
+        
+
+
+
+    }
+}
